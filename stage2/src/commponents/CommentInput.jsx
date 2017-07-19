851251510fd3cf2987c62ac240ab16cb69addcf7
wefine <wefine@aliyun.com>
2017-07-19 15:12:33 +0800

    comment input

--- stage2/src/commponents/CommentInput.jsx
+++ stage2/src/commponents/CommentInput.jsx
@@ -21,8 +21,24 @@ class CommentInput extends Component {
     // 渲染函数
     render() {
         return (
-            <div>
-                CommentInput
+            <div className="comment-input">
+                <div className="comment-field">
+                    <span className="comment-field-name">用户名：</span>
+                    <div className="comment-field-input">
+                        <input />
+                    </div>
+                </div>
+                <div className="comment-field">
+                    <span className="comment-field-name">评论内容：</span>
+                    <div className="comment-field-input">
+                        <textarea>
+
+                        </textarea>
+                    </div>
+                </div>
+                <div className="comment-field-button">
+                    <button>发布</button>
+                </div>
             </div>
         )
     }
